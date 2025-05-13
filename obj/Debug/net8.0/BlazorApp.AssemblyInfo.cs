cc50655b6eb6fdd624a35197f47c26ffba291efa
Vikram-3011 <vikramelangovan769@gmail.com>
2025-05-13 15:49:25 +0530

    wwwroot change

--- obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6297e4a2b83d71565fa1a3e06112b4e45b42820d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8eb30bddd4ed35baa976c303ab0e5fc6dfed055")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
